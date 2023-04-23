caacf61d60c13c28fa65d29e13c46cdcd4e021d3
vk0232 <128867877+vk0232@users.noreply.github.com>
2023-04-23 18:03:07 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId:         'us-west-2_H2YauVnlR',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '264598711565',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId:   '60jfubopnj2kq68240b65jq99d',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region:             'us-west-2'          // e.g. us-east-2
     },
     api: {
