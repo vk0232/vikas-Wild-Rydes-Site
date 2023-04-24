4fe907a3838b0d5726d1f7a187be903ebee7d0c6
vk0232 <128867877+vk0232@users.noreply.github.com>
2023-04-23 19:42:01 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region:             'us-west-2'          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          'https://95enq1k51g.execute-api.us-west-2.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
