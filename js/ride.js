9759154aa4e0705cafc66fd8842f291b644e74ce
vk0232 <128867877+vk0232@users.noreply.github.com>
2023-04-30 17:23:38 -0500

    Update ride.js

--- js/ride.js
+++ js/ride.js
@@ -59,6 +59,7 @@ let map;
 
         animateArrival(function animateCallback() {
             displayUpdate(unicorn.Name + ' has arrived. Giddy up!', unicorn.Color);
+            displayUpdate('Your Trip Number: 3', unicorn.Color);
             WildRydes.map.unsetLocation();
 
             $('#request').prop('disabled', 'disabled');
